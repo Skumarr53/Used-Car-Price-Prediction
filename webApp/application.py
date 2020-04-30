1cc10fa6e8fc125eee171da3f20d85727322130e
skumarr53 <skumarr53@gmail.com>
2020-04-30 19:10:41 +0530

    First Upload

---
+++ webApp/application.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, flash, request, jsonify, Markup
+import logging, io, os, sys
+import pandas as pd
+import numpy as np
+from modules.custom_transformers import *
+#from sklearn.ensemble import GradientBoostingRegressor
+import scipy
+import pickle
+
+## eb cli init
+#>../aws-elastic-beanstalk-cli-setup/scripts/bundled_installer
+#>echo 'export PATH="/home/skumar/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
+
+#Freeing up port with Port no $Port_Number
+#sudo fuser -k $Port_Number/tcp
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+np.set_printoptions(precision=2)
+
+#Model features
+gbm_model = None
+features = ['Brand', 'Model', 'Location', 'Year', 'Kilometers_Driven', 
+        'Fuel_Type', 'Transmission', 'Owner_Type', 'Mileage', 'Engine', 
+        'Power', 'Seats']
+
+
+@application.before_first_request
+def startup():
+
+    global gbm_model, model2brand
+    
+    # gbm model
+    with open('static/GBM_Regressor_pipeline.pkl', 'rb') as f:
+        gbm_model = pickle.load(f)
+
+        # min, max, default values to categories mapping dictionary
+    with open('static/Dictionaries.pkl', 'rb') as f:
+        default_dict,min_dict, max_dict, default_dict_mapped = pickle.load(f)
+
+    # Encoded values to categories mapping dictionary
+    with open('static/Encoded_dicts.pkl', 'rb') as f:
+        le_brands_Encdict,le_models_Encdict,le_locations_Encdict,le_fuel_types_Encdict,le_transmissions_Encdict,le_owner_types_Encdict = pickle.load(f)
+
+    with open('static/model2brand.pkl', 'rb') as f:
+        model2brand = pickle.load(f)
+
+@application.errorhandler(500)
+def server_error(e):
+    logging.exception('some eror')
+    return """
+    And internal error <pre>{}</pre>
+    """.format(e), 500
+
+@application.route("/", methods=['POST', 'GET'])
+def index():
+     # Encoded values to categories mapping dictionary
+      # Encoded values to categories mapping dictionary
+    with open('static/Encoded_dicts.pkl', 'rb') as f:
+        le_brands_Encdict,le_models_Encdict,le_locations_Encdict,le_fuel_types_Encdict,le_transmissions_Encdict,le_owner_types_Encdict = pickle.load(f)
+
+
+    return render_template( 'index.html', model2brand = model2brand,le_models_Encdict = le_models_Encdict,le_locations_Encdict = le_locations_Encdict, le_fuel_types_Encdict = le_fuel_types_Encdict, le_transmissions_Encdict = le_transmissions_Encdict, le_owner_types_Encdict = le_owner_types_Encdict, le_brands_Encdict = le_brands_Encdict,price_prediction = 17.09)
+
+
+
+# accepts either deafult values or user inputs and outputs prediction 
+@application.route('/background_process', methods=['POST', 'GET'])
+def background_process():
+    Brand = request.args.get('Brand')                                        
+    Model = request.args.get('Model')                                        
+    Location = request.args.get('Location')
+    Year = int(request.args.get('Year'))                                          
+    Kilometers_Driven = float(request.args.get('Kilometers_Driven'))                
+    Fuel_Type = request.args.get('Fuel_Type')
+    Transmission = request.args.get('Transmission')
+    Owner_Type = request.args.get('Owner_Type')
+    Mileage = float(request.args.get('Mileage'))                                    
+    Engine = float(request.args.get('Engine'))                                      
+    Power = float(request.args.get('Power'))                                        
+    Seats = float(request.args.get('Seats'))
+
+	# values stroed in list later to be passed as df while prediction
+    user_vals = [Brand, Model, Location, Year, Kilometers_Driven, 
+        Fuel_Type, Transmission, Owner_Type, Mileage, Engine, 
+        Power, Seats]
+
+
+    x_test_tmp = pd.DataFrame([user_vals],columns = features)
+    float_formatter = "{:.2f}".format
+
+    pred = float_formatter(np.exp(gbm_model.predict(x_test_tmp[features])[0]))
+    return jsonify({'price_prediction':pred})
+
+# when running app locally
+if __name__ == '__main__':
+    application.debug = False
+    application.run()
---
+++ webApp/modules/custom_transformers.py
@@ -0,0 +1,445 @@
+# Reference
+# http://zacstewart.com/2014/08/05/pipelines-of-featureunions-of-pipelines.html
+
+import numpy as np
+import pandas as pd
+from sklearn.cluster import DBSCAN
+from functools import reduce
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import SelectKBest, mutual_info_regression, RFE, RFECV
+from sklearn.linear_model import LogisticRegression
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import FunctionTransformer, StandardScaler, RobustScaler, MultiLabelBinarizer
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
+
+class DFFunctionTransformer(BaseEstimator, TransformerMixin):
+    # FunctionTransformer but for pandas DataFrames
+
+    def __init__(self, *args, **kwargs):
+        self.ft = FunctionTransformer(*args, **kwargs)
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        Xt = self.ft.transform(X)
+        Xt = pd.DataFrame(Xt, index=X.index, columns=X.columns)
+        return Xt
+
+
+class DFFeatureUnion(BaseEstimator, TransformerMixin):
+    # FeatureUnion but for pandas DataFrames
+
+    def __init__(self, transformer_list):
+        self.transformer_list = transformer_list
+
+    def fit(self, X, y=None):
+        for (name, t) in self.transformer_list:
+            t.fit(X, y)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xts = [t.transform(X) for _, t in self.transformer_list]
+        Xunion = reduce(lambda X1, X2: pd.merge(X1, X2, left_index=True, right_index=True), Xts)
+        return Xunion
+
+
+class DFImputer(BaseEstimator, TransformerMixin):
+    # Imputer but for pandas DataFrames
+
+    def __init__(self, strategy='mean'):
+        self.strategy = strategy
+        self.imp = None
+        self.statistics_ = None
+
+    def fit(self, X, y=None):
+        self.imp = Imputer(strategy=self.strategy)
+        self.imp.fit(X)
+        self.statistics_ = pd.Series(self.imp.statistics_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Ximp = self.imp.transform(X)
+        Xfilled = pd.DataFrame(Ximp, index=X.index, columns=X.columns)
+        return Xfilled
+
+
+class DFStandardScaler(BaseEstimator, TransformerMixin):
+    # StandardScaler but for pandas DataFrames
+
+    def __init__(self):
+        self.ss = None
+        self.mean_ = None
+        self.scale_ = None
+
+    def fit(self, X, y=None):
+        self.ss = StandardScaler()
+        self.ss.fit(X)
+        self.mean_ = pd.Series(self.ss.mean_, index=X.columns)
+        self.scale_ = pd.Series(self.ss.scale_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xss = self.ss.transform(X)
+        Xscaled = pd.DataFrame(Xss, index=X.index, columns=X.columns)
+        return Xscaled
+
+
+class DFRobustScaler(BaseEstimator, TransformerMixin):
+    # RobustScaler but for pandas DataFrames
+
+    def __init__(self):
+        self.rs = None
+        self.center_ = None
+        self.scale_ = None
+
+    def fit(self, X, y=None):
+        self.rs = RobustScaler()
+        self.rs.fit(X)
+        self.center_ = pd.Series(self.rs.center_, index=X.columns)
+        self.scale_ = pd.Series(self.rs.scale_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xrs = self.rs.transform(X)
+        Xscaled = pd.DataFrame(Xrs, index=X.index, columns=X.columns)
+        return Xscaled
+
+
+class ColumnExtractor(BaseEstimator, TransformerMixin):
+
+    def __init__(self, cols):
+        self.cols = cols
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xcols = X[self.cols]
+        return Xcols
+
+
+class ZeroFillTransformer(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xz = X.fillna(value=0)
+        return Xz
+
+
+
+class Log1pTransformer(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xlog = np.log1p(X)
+        return Xlog
+
+
+class DateFormatter(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xdate = X.apply(pd.to_datetime)
+        return Xdate
+
+
+class DateDiffer(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        beg_cols = X.columns[:-1]
+        end_cols = X.columns[1:]
+        Xbeg = X[beg_cols].as_matrix()
+        Xend = X[end_cols].as_matrix()
+        Xd = (Xend - Xbeg) / np.timedelta64(1, 'D')
+        diff_cols = ['->'.join(pair) for pair in zip(beg_cols, end_cols)]
+        Xdiff = pd.DataFrame(Xd, index=X.index, columns=diff_cols)
+        return Xdiff
+
+
+class DummyTransformer(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        self.dv = None
+
+    def fit(self, X, y=None):
+        # assumes all columns of X are strings
+        Xdict = X.to_dict('records')
+        self.dv = DictVectorizer(sparse=False)
+        self.dv.fit(Xdict)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xdict = X.to_dict('records')
+        Xt = self.dv.transform(Xdict)
+        cols = self.dv.get_feature_names()
+        Xdum = pd.DataFrame(Xt, index=X.index, columns=cols)
+        # drop column indicating NaNs
+        nan_cols = [c for c in cols if '=' not in c]
+        Xdum = Xdum.drop(nan_cols, axis=1)
+        return Xdum
+
+
+class MultiEncoder(BaseEstimator, TransformerMixin):
+    # Multiple-column MultiLabelBinarizer for pandas DataFrames
+
+    def __init__(self, sep=','):
+        self.sep = sep
+        self.mlbs = None
+
+    def _col_transform(self, x, mlb):
+        cols = [''.join([x.name, '=', c]) for c in mlb.classes_]
+        xmlb = mlb.transform(x)
+        xdf = pd.DataFrame(xmlb, index=x.index, columns=cols)
+        return xdf
+
+    def fit(self, X, y=None):
+        Xsplit = X.applymap(lambda x: x.split(self.sep))
+        self.mlbs = [MultiLabelBinarizer().fit(Xsplit[c]) for c in X.columns]
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xsplit = X.applymap(lambda x: x.split(self.sep))
+        Xmlbs = [self._col_transform(Xsplit[c], self.mlbs[i])
+                 for i, c in enumerate(X.columns)]
+        Xunion = reduce(lambda X1, X2: pd.merge(X1, X2, left_index=True, right_index=True), Xmlbs)
+        return Xunion
+
+
+class StringTransformer(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xstr = X.applymap(str)
+        return Xstr
+
+
+class ClipTransformer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, a_min, a_max):
+        self.a_min = a_min
+        self.a_max = a_max
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xclip = np.clip(X, self.a_min, self.a_max)
+        return Xclip
+
+
+class AddConstantTransformer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, c=1):
+        self.c = c
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xc = X + self.c
+        return Xc
+
+
+class ShiftTranformer(BaseEstimator, TransformerMixin):
+    
+    def __init__(self, par = 1):
+        self.par = par
+
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        X_sub = self.par - X 
+        return X_sub
+
+
+
+
+class DFRecursiveFeatureSelector(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,estimator=DecisionTreeRegressor(), n_features = 10, step=10):
+        self.estimator = estimator
+        self.n_features = n_features
+        self.step = step
+        
+    def fit(self, X, y=None):
+        # stateless transformer
+        self.RFE = RFE(estimator=self.estimator, n_features_to_select=self.n_features, step=self.step)
+        self.RFE.fit(X,y)
+        return self
+
+    def transform(self, X):
+        return X.loc[:,self.RFE.get_support()]
+
+       
+
+
+class DFSelectKBest(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,estimator=mutual_info_regression, n_features = 10):
+        self.estimator = estimator
+        self.n_features = n_features
+        
+    def fit(self, X, y=None):
+        # stateless transformer
+        self.SelectKBest = SelectKBest(self.estimator, k=self.n_features)
+        self.SelectKBest.fit(X,y)
+        return self
+
+    def transform(self, X):
+        return X.loc[:,self.SelectKBest.get_support()]
+
+class DFembedFeatureSelection(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,estimator=DecisionTreeRegressor(), n_features = 10):
+        self.estimator = estimator
+        self.n_features = n_features
+        
+    def fit(self, X, y=None):
+        # stateless transformer
+        self.estimator.fit(X,y)
+        return self
+
+    def transform(self, X):
+        ind = self.estimator.feature_importances_.argsort()[-self.n_features:][::-1]
+        return X.iloc[:,ind]
+
+class DFadd_ColInteraction(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,col1,col2):
+        self.col1 = col1
+        self.col2 = col2
+        
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        X[f'{self.col1}_&_{self.col2}'] = X[self.col1]*X[self.col2]
+        return X
+
+class DFremoveOutlier_IQR(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,factor = 1.5):
+        self.factor = factor
+
+        
+    def fit(self, X, y=None):
+        # stateless transformer
+        self.y = y
+        return self
+
+    def transform(self, X):
+
+        Q1 = X.quantile(0.25)
+        Q3 = X.quantile(0.75)
+        IQR = Q3 - Q1
+
+        inds = ~((X < (Q1 - self.factor * IQR)) |(X > (Q3 + self.factor * IQR))).any(axis=1)
+        X_screened = X[inds]
+        self.y = self.y[inds]
+        return X_screened, self.y
+
+class DF_RFECV_FeatureSelection(BaseEstimator, TransformerMixin):
+
+    def __init__(self,estimator = DecisionTreeRegressor(), cv = StratifiedKFold(3),step = 1, scoring = 'r2'):
+        self.estimator = estimator
+        self.scoring = scoring
+        self.step = step
+        self.cv = cv
+
+    def fit(self,X, y=None):
+
+        self.rfevc = RFECV(estimator = self.estimator,step=self.step, cv=self.cv, scoring=self.scoring)
+        self.fit(X,y)
+        return self
+
+    def transform(self,X):
+        return X.drop(X.columns[np.where(self.rfevc.support_ == False)[0]], axis=1, inplace=True)
+
+class DFdrop_Cols(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,cols = None):
+        self.cols = cols
+        
+    def fit(self, X, y=None):
+        # stateless transformer
+        return self
+
+    def transform(self, X):
+        X.drop(self.cols, axis=1, inplace=True)
+        return X
+
+class DF_Model(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,estimator = None):
+        self.estimator = estimator
+
+    def fit(self,X,y=None):
+        self.estimator.fit(X,y)
+        return self
+
+    def predict(self,X):
+        preds = self.estimator.predict(X)
+        return preds
+
+class DF_PolynomialFeatures(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,degree = 2, interaction= True):
+        self.degree = degree
+        self.interaction = interaction
+    
+    def fit(self,X,y=None):
+        self.poly = PolynomialFeatures(degree=self.degree
+                                       ,interaction_only=self.interaction)
+        self.poly.fit(X)
+        return self
+
+    def transform(self,X):
+        X_poly = self.poly.transform(X)
+        return pd.DataFrame(X_poly,index=X.index,columns=self.poly.get_feature_names(X.columns))
+
+
